43f9103a5fa7b7aaddd90f4e34f62e667bb7ff97
yug201 <yugkaushik201@gmail.com>
2026-09-09 20:47:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import requests
 from datetime import datetime
 import os
 import os
-BASE_URL = os.getenv("BASE_URL", "http://localhost:8000")
+# BASE_URL = os.getenv("BASE_URL", "http://localhost:8000")
+BASE_URL="https://knowledgevault-3bqk.onrender.com/"
 st.set_page_config(page_title="Vault Dashboard", layout="wide", page_icon="🛡️")
 
 # --- IMPROVED CSS (Fixes the white bars and improves spacing) ---
